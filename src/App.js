58a534af4e5b7bb7d90ceedbf5b2a184ea80d2f3
Alejandro Fiesco <alfiesparra@hotmail.com>
2023-05-09 12:40:10 -0500

    Con register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Route } from 'wouter';
 import Login from './pages/login/Login';
-import RegisterForm from './components/forms/register/Register.form';
 import Register from './pages/register/register';
 
 function App() {
