a7cb7e46b160ad9ff5d5b6e254f73f0941de0610
Alejandro Fiesco <alfiesparra@hotmail.com>
2023-05-09 23:10:55 -0500

    Se quitan referencias sin uso

--- src/pages/register/Registrar.jsx
+++ src/pages/register/Registrar.jsx
@@ -1,24 +1,18 @@
-import { useRef } from "react";
 import RegisterForm from "../../components/forms/register/Register.form"
 import CreditCardForm from "../../components/forms/register/CreditCard.form";
 import '../styles/forms-inner.css';
 import '../styles/forms.css';
 
 export default function Register() {
-  const txtUser = useRef(null);
-  const txtPass = useRef(null);
 
   function handleSubmit(e) {
-    e.prevent.default();
-    const user = txtUser.current.value;
-    const pass = txtPass.current.value;
-  }
+    e.prevent.default();  }
 
   return (
     <div className="bg single-element">
       <form className="form-layout" onSubmit={handleSubmit} >
         <div className="form-container register">
-          <RegisterForm txtUser={txtUser} txtPass={txtPass} />
+          <RegisterForm />
           <div className="img degraded-black">
             <CreditCardForm />
           </div>
