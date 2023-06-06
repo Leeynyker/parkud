ace4ee7bcf616b169f1698bcf6c3843cacb9535d
leeynyker <leeynykeramr@gmail.com>
2023-06-05 21:38:55 -0500

    booking1

--- src/components/parking-card/Parking-card.jsx
+++ src/components/parking-card/Parking-card.jsx
@@ -1,29 +1,85 @@
-import Map from '../maps/Map';
-import './parking-card.css';
-import { Link } from 'wouter';
-// import React, { useEffect, useState } from 'react';
-export default function ParkingCard({ id, nombre, direccion, cupos, tarifa, apertura, cierre, img, puntos = null }) {
-  // const address = '1600 Amphitheatre Parkway, Mountain View, CA';
-  
+import Map from "../maps/Map";
+import "./parking-card.css";
+import { Link } from "wouter";
+import { Modal, Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import Booking from "../booking/Booking";
+
+export default function ParkingCard({
+  id,
+  nombre,
+  direccion,
+  cupos,
+  tarifa,
+  apertura,
+  cierre,
+  img,
+  puntos = null,
+}) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const handleOpenModal = () => {
+    setModalData({
+      id,
+      nombre,
+      direccion,
+      cupos,
+      tarifa,
+      apertura,
+      cierre,
+      img,
+      puntos,
+    });
+    setModalOpen(true);
+  };
+
   return (
     <div className="parking-card">
       <div className="parking-info">
         <div className="main-info">
-          <Link to={`/parking/${id}`}><h4>{nombre}</h4></Link>
+          <Link to={`/parking/${id}`}>
+            <h4>{nombre}</h4>
+          </Link>
           <span>{direccion}</span>
         </div>
         <span>{cupos} cupos libres</span>
         <span>Tarifa: ${tarifa}</span>
         <span>{`${apertura} - ${cierre}`}</span>
-        {puntos &&
+        {puntos && (
           <span className="puntos">{`Puntos PAR-KUD: ${puntos}`}</span>
-        }
+        )}
+        <button className="degraded-orange button-booking" onClick={handleOpenModal}>Reservar</button>
       </div>
       <div className="parking-img single-element">
         <div className="simulador-img">
-          <Map direccion={direccion}/>
+          <Map direccion={direccion} />
         </div>
       </div>
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)} style={{ width: '800px' }}>
+        {modalData && (
+          <>
+            <Modal.Header>{modalData.nombre}</Modal.Header>
+            <Modal.Content>
+              <Booking
+                nombre={nombre}
+                direccion={direccion}
+                cupos={cupos}
+                tarifa={tarifa}
+                apertura={apertura}
+                cierre={cierre}
+                puntos={puntos}
+              />
+              {/* <p>Dirección: {modalData.direccion}</p> */}
+              {/* Agrega el resto de los datos de la tarjeta aquí */}
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={() => setModalOpen(false)}>Cerrar</Button>
+              {/* Agrega el botón de reserva aquí */}
+            </Modal.Actions>
+          </>
+        )}
+      </Modal>
     </div>
-  )
+  );
 }
