c53d1adcb39e03f1d0a597885b629c247b75084d
CASTVELL <lmcastellanosa@correo.udistrital.edu.co>
2023-06-07 21:13:01 -0500

    x

--- src/components/sidebarStats/SidebarStats.jsx
+++ src/components/sidebarStats/SidebarStats.jsx
@@ -18,7 +18,7 @@ const { allowedMaxDays, allowedDays, allowedRange, beforeToday, afterToday, comb
 
 export default function SidebarStats({sidebarData}) {
 
-  const [callDataExterno, setcallDataExterno] = useState(false);
+  const [callDataExterno, setcallDataExterno] = useState(null);
 
   const nombresParqueadero = sidebarData.map((item) => item.nombreParqueadero);
   console.log("hey2: "+nombresParqueadero)
@@ -34,17 +34,17 @@ export default function SidebarStats({sidebarData}) {
   return (
     <section className="sidebar" >
       <br></br>
+      <Link href="">{nombresParqueadero[callDataExterno]}</Link>
       <br></br>
       <br></br>
       <br></br>
-      <br></br>
-      <Link href={""}>Parqueadero</Link>
+      <Link href={""}>Seleccione Parqueadero</Link>
       <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <OptionButton data={sidebarData} callback={handleCallback} size="lg"></OptionButton>
+      <OptionButton data={sidebarData} callback={handleCallback} size='lg'></OptionButton>
       </div>
       
-      <Link href={""}>Fecha</Link>
-      <DateRangePicker disabledDate={allowedMaxDays(7)} character=' hasta ' format='dd/MM/yy' size='md' />
+      <Link href={""}>Seleccione Fecha</Link>
+      <DateRangePicker shouldDisableDate ={allowedMaxDays(7)} character=' hasta ' format='dd/MM/yy' size='md' />
       <br></br>
       
       <Button color="orange" appearance="primary">Search</Button>
--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -29,34 +29,54 @@ export default function Stats( ) {
     const [ventasData, setVentasData] = useState([null]);
 
     useEffect(() => {
-      
+      //Aqui se conecta con el backend para traer los datos de ventas
+      //y guardarlos en ventasData
     });
 
 
 
 
     const barChartData = {
-      "01/02/2023": "80000",
+      "01/02/2023": "40000",
       "02/02/2023": "10000",
-      "03/02/2023": "200000",
-      "04/02/2023": "100000",
-      "05/02/2023": "5000"
+      "03/02/2023": "25000",
+      "04/02/2023": "30000",
+      "05/02/2023": "5000",
+      "06/02/2023": "10000",
+      "07/02/2023": "20000"
     };
 
     const barkeys = Object.keys(barChartData);
     const barvalues = Object.values(barChartData);
+    var ventasTotales = null;
+    for(var i = 0; i < barvalues.length; i++){
+
+      ventasTotales += parseInt(barvalues[i]);
+    }
   
+
+
     const pieChartData = {
       "01/02/2023": "40",
       "02/02/2023": "1",
       "03/02/2023": "27",
       "04/02/2023": "13",
-      "05/02/2023": "59"
+      "05/02/2023": "59",
+      "06/02/2023": "10",
+      "07/02/2023": "20"
     };
 
     const piekeys = Object.keys(pieChartData);
     const pievalues = Object.values(pieChartData);
-    
+
+    var tasaOcupacion = null;
+    for(var i = 0; i < pievalues.length; i++){
+
+      tasaOcupacion += parseInt(pievalues[i]);
+    }
+    tasaOcupacion= tasaOcupacion/pievalues.length;
+
+
 
     const[barChartConfig, charUserData] = useState({
         labels: barkeys,
@@ -92,16 +112,16 @@ export default function Stats( ) {
               <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft:"100px", padding:"10px"  }} >
                 <div style={{ margin:"10px", padding: '20px', width:"90%", height: '600px', background: 'white', borderRadius: '10px', border: '1px solid gray', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'  }}>
                   
-                  <div >Ingresos Movistar Arena | 01/01/2023 - 05/02/2023</div>
-                  <div style={{ padding: '4px'}}>395,000 COP</div>
+                  <div >Ingresos Brutos</div>
+                  <div style={{ padding: '4px'}}>{ventasTotales} COP</div>
                   <BarChart chartData={barChartConfig} />
 
                 </div>
 
                 <div style={{ margin:"10px", padding: '20px', width:"90%", height: '450px', background: 'white', borderRadius: '10px', border: '1px solid gray', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'  }}>
                   
-                  <div>Ingresos Movistar Arena | 01/01/2023 - 05/02/2023</div>
-                  <div style={{ padding: '2px'}}>28%</div>
+                  <div>Tasa de Ocupación</div>
+                  <div style={{ padding: '2px'}}>{tasaOcupacion}%</div>
                   <PieChart chartData={pieChartConfig} style={{width:"50%"}}/>
 
                 </div>
