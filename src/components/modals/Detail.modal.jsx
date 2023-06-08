6431865335914de0cd88eb28ddbace980855b586
Leeynker <lmontano@ieee.com>
2023-06-08 14:21:57 -0500

    pss

--- src/components/modals/Detail.modal.jsx
+++ src/components/modals/Detail.modal.jsx
@@ -1,30 +1,32 @@
 import { Modal, ModalContent, ModalHeader, Button } from "semantic-ui-react";
 
 export default function Detail({ horario, tarifa, setModalOpen, modalOpen }) {
-
-  let total = ''
+  let total = "";
   if (horario) {
-    const temp = horario.split('/')[1].split('-')
-    let horaEntrada = temp[0]
-    let horaSalida = temp[1]
-    horaEntrada = new Date('2000-01-01 ' + horaEntrada).getTime()
-    horaSalida = new Date('2000-01-01 ' + horaSalida).getTime()
+    const temp = horario.split("/")[1].split("-");
+    let horaEntrada = temp[0];
+    let horaSalida = temp[1];
+    horaEntrada = new Date("2000-01-01 " + horaEntrada).getTime();
+    horaSalida = new Date("2000-01-01 " + horaSalida).getTime();
     total = (horaSalida - horaEntrada) * (tarifa / (60 * 60 * 1000));
   }
-
-  <Modal open={modalOpen} onClose={() => setModalOpen(false)} style={{ width: '400px' }}>
-    <Modal.Header>
-      Total a pagar
-    </Modal.Header>
-    <Modal.Content>
-      <>
-        <h3>Total:</h3>
-        <h4>{total}</h4>
-      </>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button onClick={() => setModalOpen(false)}>Cerrar</Button>
-      {/* Agrega el botón de reserva aquí */}
-    </Modal.Actions>
-  </Modal>
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={() => setModalOpen(false)}
+      style={{ width: "400px" }}
+    >
+      <Modal.Header>Total a pagar</Modal.Header>
+      <Modal.Content>
+        <>
+          <h3>Total:</h3>
+          <h4>{total}</h4>
+        </>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={() => setModalOpen(false)}>Cerrar</Button>
+        {/* Agrega el botón de reserva aquí */}
+      </Modal.Actions>
+    </Modal>
+  );
 }
